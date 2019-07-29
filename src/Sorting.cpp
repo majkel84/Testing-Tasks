184a9bc552fa8c5f8b4c32ee7ed53c1b9777df23
majkel <michal.sobotka.84@gmail.com>
2019-07-29 22:13:23 +0200

    Update Sorting.cpp

--- src/Sorting.cpp
+++ src/Sorting.cpp
@@ -60,10 +60,10 @@ int Sorting::MaxProductOfThree(vector<int> &A)
 
 int Sorting::NumberOfDiscIntersection(vector<int> &A)
 {
-    int solution = 0;
+    int j, solution = 0;
 
     for (auto i = A.size()-1; i >= 1; i--)
-        for (int j = i-1; j >= 0; j--)
+        for (j = i-1; j >= 0; j--)
         {
             if (solution >= 10000000)
                 return -1;
